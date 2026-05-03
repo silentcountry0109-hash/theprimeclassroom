51a1ec1db470618c5de20cf439be7c7f099d2d72
allendanlu <41358476-allendanlu@users.noreply.replit.com>
2026-05-03 13:04:11 +0800

    任務 #176：替換 Hero 區副標題 GIF
    - client/src/pages/landing.tsx：將 subtitleGif 的 import 來源從舊檔案
      `Adobe_Express_-_國小個別指導眼睛_1776335060502.gif`
      改為新檔案 `國小個別指導_1777784388983.gif`
    - PingPongGif 元件邏輯、splashGif、heroLogoImg 均不變
    - 新 GIF 維持原本的 ping-pong 播放效果

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -86,7 +86,7 @@ import deco15Img from "@assets/工作區域_15_1776423698456.png";
 import deco10Img from "@assets/工作區域_10_1776423709223.png";
 import deco6Img from "@assets/工作區域_6_1776423712973.png";
 import deco5Img from "@assets/工作區域_5_1776423714787.png";
-import subtitleGif from "@assets/Adobe_Express_-_國小個別指導眼睛_1776335060502.gif";
+import subtitleGif from "@assets/國小個別指導_1777784388983.gif";
 import teachImg1 from "@assets/螺旋式課程_1776335067651.png";
 import teachImg2 from "@assets/階梯式教學_1776335069419.png";
 import teachImg3 from "@assets/單元評測_1776335072659.png";
